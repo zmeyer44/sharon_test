f13d72a5bdd5d340de0e96d5fcab1eb539403aae
zmeyer44 <meyerz@wustl.edu>
2020-05-04 20:13:54 -0400

    for meeting

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div className="container col-12">
+            <div className="container col-12 mx-0" id="full">
                 <div className="row hero">
                     <div className="container hero-inner">
                         <div className="row ">
@@ -15,16 +15,30 @@ export default class Home extends Component {
                     </div>
 
                 </div>
-                <div className="row align-items-center">
-                    <div className="col-4 mx-auto">
+
+                <div className="row align-items-center mx-auto" id="bar2">
+                    <div className="col-10 col-md-4 mx-auto">
+                        <h5>We are dedicated to</h5>
                         <h2>Helping our community</h2>
                         <p>We are dedicated to helping all members or the floral park bellrose school comminuity who are struggling through these troubling times. We encourage all those who are in need of support to utilize all of the available resources to make the most of your child's learning experience.</p>
 
                     </div>
-                    <div className="col-4 mx-auto">
-                    <img className="py-4" src={require('../logo111.jpg')} width="300px"></img>
+                    <div className="col-10 col-md-4 mx-auto">
+                    <img className="py-4 mx-auto" src={require('../logo111.jpg')} width="300px"></img>
 
                     </div>
+                </div>
+                <div className="row align-items-center mx-0 col-12" id="bar3">
+                    <div className="container">
+                        <div className="row">
+                        <div className="circle align-items-center">
+                        <i className="fas fa-search fa-4x pin"></i>
+                        </div>
+                        </div>
+
+
+                    </div>
+                    
                 </div>
             </div>
         )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class  extends Component {
     render() {
         return (
-            <div className="container mx-0 col-12 p-0">
+            <div className="container mx-0 col-12">
                 <div className="row">
                     <a className="alertbar" href="https://nycwell.cityofnewyork.us/en/covid-19-digital-mental-health-resources/">
                         <div className="alert-bar-inner container text-center p-2">
@@ -22,11 +22,33 @@ export default class  extends Component {
                     <i className="fas fa-bars fa-2x mr-3 bars"></i>
                 </nav>
                 <div className="row expandable">
-                    <ul className="text-purple">
-                        <li>Find Services</li>
-                        <li>Coping and Wellness Tips</li>
-                        <li>Crisis Services</li>
-                        <li>For Providers</li>
+                    <ul className="text-purple" id="topbar">
+                        <li><a href="">Find Services</a></li>
+                        <li><a href="">App Library</a></li>
+                        <li className="dropdown"><a href="">Coping and Wellness Tips</a>
+                        <ul>
+                            <li><a href="">Tips for staying mentaly healthy</a></li>
+                            <li><a href="">Know when your stressed</a></li>
+                            <li><a href="">Take a deep breath</a></li>
+                            <li><a href="">Full body relaxation</a></li>
+                            <li><a href="">Bring mindfulness to life</a></li>
+                        </ul>
+                        </li>
+                        <li className="dropdown"><a href="">Crisis Services</a>
+                        <ul>
+                            <li><a href="">Know who to call</a></li>
+                            <li><a href="">Have a safety plan</a></li>
+                            <li><a href="">Helping friends or family in a crisis</a></li>
+                            <li><a href="">Mobile Crisis Teams</a></li>
+                        </ul>
+                        </li>
+                        <li className="dropdown"><a href="">For Providers</a>
+                        <ul>
+                            <li><a href="">Healthly Homes</a></li>
+                            <li><a href="">Home and community based services</a></li>
+                            <li><a href="">Provider updates and additions</a></li>
+                        </ul>
+                        </li>
                     </ul>
                 </div>
             
